d182c2b2337d03bd77987a6eedb1ed112e739752
Spdrd <juandiegoreyesrodriguez@gmail.com>
2026-02-19 20:24:55 -0500

    api working

--- app/api/v1/chatbot.py
+++ app/api/v1/chatbot.py
@@ -1,18 +1,14 @@
-from dns import message
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.core.dependencies import get_openai_service
 from app.schemas.chatbot import ChatRequest
-
-from fastapi.security import OAuth2PasswordBearer
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+from app.core.security import verify_api_key
 
 router = APIRouter()
 
 @router.post("/chat", status_code=status.HTTP_200_OK)
 def chat(
     request: ChatRequest,
-    token: str = Depends(oauth2_scheme),
+    _: str = Depends(verify_api_key),
     service = Depends(get_openai_service)):
     response = service.chat(request.message)
-
     return {"response": response}
--- app/client/openai_client.py
+++ app/client/openai_client.py
@@ -1,14 +1,35 @@
 from openai import OpenAI
 from app.core.config import OPENAI_API_KEY
-class OpenAIClient():
+
+
+class OpenAIClient:
 
     def __init__(self):
         self.client = OpenAI(api_key=OPENAI_API_KEY)
 
+        # 🔥 Conocimiento base del bot
+        self.system_prompt = """
+        Eres un asistente virtual de la Inmobiliaria Marckpluss.
+        Respondes de forma clara, profesional y concisa.
+        Información de la empresa:
+        - Ofrecemos Inmuebles
+        
+        Responde solo con base en esta información.
+        """
+
     def send_message(self, message: str) -> str:
         response = self.client.responses.create(
             model="gpt-4.1-mini",
-            input=message
+            input=[
+                {
+                    "role": "system",
+                    "content": self.system_prompt
+                },
+                {
+                    "role": "user",
+                    "content": message
+                }
+            ]
         )
 
         return response.output_text
